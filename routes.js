f86d4e5060eb4531899e6774e21deb6c1fbadb09
pablohpaiva22 <pablohpaiva22@gmail.com>
2021-11-23 06:50:09 -0300

    Ajuste de rota

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const routes = express.Router()
 const functions = require("./functions")
 
 routes.get("/", function(req, res) {
-    return res.render("layout")
+    return res.redirect("instructors")
 })
 
 routes.get("/instructors", functions.instructor)
